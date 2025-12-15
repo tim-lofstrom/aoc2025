7f7aee1317fc9f616b6cddb4938da35c3e4a232e
Tim Löfström Danielsson <tim.lofstrom.danielsson@lm.se>
2025-12-15 23:02:06 +0100

    update

--- day7/index.ts
+++ day7/index.ts
@@ -1,4 +1,4 @@
-import { first, last, map, max, maxBy, range, reduce, replace, rest, reverse, sortBy, split, sum, tail, union } from "lodash";
+import { map, reduce } from "lodash";
 import { input } from "./input";
 import { example } from "./example";
 
@@ -32,7 +32,7 @@ function replaceInGrid(grid: string[][], [x, y]: Point, value: string): string[]
   return map(grid, (row, rowIndex) => (rowIndex !== y ? row : map(row, (cell, colIndex) => (colIndex === x ? value : cell))));
 }
 
-function shootBeam(grid: string[][], start: Point): [string[][], number] {
+function shootBeamCountSplit(grid: string[][], start: Point): [string[][], number] {
   const [x, y] = beamDirectionNext(start);
 
   if (y > grid[0].length) {
@@ -41,23 +41,62 @@ function shootBeam(grid: string[][], start: Point): [string[][], number] {
 
   if (grid[y][x] === ".") {
     const down = replaceInGrid(grid, [x, y], "|");
-    return shootBeam(down, [x, y]);
+    return shootBeamCountSplit(down, [x, y]);
   } else if (grid[y][x] === "^") {
     const left = replaceInGrid(grid, [x - 1, y], "|");
-    const [leftShot, leftCount] = shootBeam(left, [x - 1, y]);
+    const [leftShot, leftCount] = shootBeamCountSplit(left, [x - 1, y]);
     const right = replaceInGrid(leftShot, [x + 1, y], "|");
-    const [rightShot, rightCount] = shootBeam(right, [x + 1, y]);
+    const [rightShot, rightCount] = shootBeamCountSplit(right, [x + 1, y]);
     return [rightShot, leftCount + rightCount + 1];
   }
 
   return [grid, 0];
 }
 
+const keyOf = ([x, y]: Point) => `${x},${y}`;
+
+function beamTimelinesMemoized(grid: string[][], start: Point, memo: Map<string, number>): number {
+  const key = keyOf(start);
+  return memo.has(key) ? memo.get(key)! : beamTimelines(grid, start, memo, key);
+}
+function beamTimelines(grid: string[][], start: Point, memo: Map<string, number>, key: string) {
+  const [x, y] = beamDirectionNext(start);
+  if (y > grid[0].length) {
+    memo.set(key, 1);
+    return 1;
+  }
+  const result = shootBeam(grid, y, x, memo, key);
+  memo.set(key, result);
+  return result;
+}
+
+function shootBeam(grid: string[][], y: number, x: number, memo: Map<string, number>, key: string) {
+  if (grid[y][x] === ".") {
+    return beamTimelinesMemoized(grid, [x, y], memo);
+  } else if (grid[y][x] === "^") {
+    const leftCount = beamTimelinesMemoized(grid, [x - 1, y], memo);
+    const rightCount = beamTimelinesMemoized(grid, [x + 1, y], memo);
+    return leftCount + rightCount;
+  } else if (grid[y][x] === "|") {
+    return 0;
+  } else {
+    throw new Error("Unexpected grid value");
+  }
+}
+
 function part1(data: string[]) {
   let grid = data.map((line) => line.split(""));
   const start = findStart(grid, "S");
-  const [manifold, splitCount] = shootBeam(grid, start);
+  const [_manifold, splitCount] = shootBeamCountSplit(grid, start);
   return splitCount;
+}
+
+function part2(data: string[]) {
+  let grid = data.map((line) => line.split(""));
+  const memo = new Map<string, number>();
+  const start = findStart(grid, "S");
+  const timlines = beamTimelinesMemoized(grid, start, memo);
+  return timlines;
 }
 
 console.log("Day 7: Laboratories");
@@ -65,3 +104,5 @@ console.log("---------");
 console.log("Part 1");
 console.log("Example: " + part1(example));
 console.log("Input: " + part1(input));
+console.log("Example: " + part2(example));
+console.log("Example: " + part2(input));
